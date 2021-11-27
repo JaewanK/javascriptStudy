0bb46d7335acba8f76b2ba0eade0e8c1ac51c644
JaewanK <37770843+JaewanK@users.noreply.github.com>
2021-11-27 19:34:54 +0900

    Update chap1-4_dogYear.js

--- chap1-4_dogYear.js
+++ chap1-4_dogYear.js
@@ -1,6 +1,6 @@
 // 개 나이를 구해보자
 
-let myAge = 31; // 내나이
+const myAge = 31; // 내나이
 
 let earlyYears = 2; // 전기 나이 2년
 earlyYears *= 10.5; // 10.5를 곱하고 reassign 
@@ -12,6 +12,6 @@ laterYears *= 4;
 
 myAgeInDogYears = earlyYears + laterYears; // 전기 후기 나이 더하기
 
-let myName = 'JAYK'.toLowerCase() //내이름을 소문자로 표시
+const myName = 'JAYK'.toLowerCase() //내이름을 소문자로 표시
 
 console.log(`내이름은 ${myName}입니다. 사람나이로는 ${myAge}입니다. 개나이로는 ${myAgeInDogYears}이에요.`)
