bca6e2ab641f6c8a6339b89132173e2317aac00f
JaewanK <37770843+JaewanK@users.noreply.github.com>
2021-11-27 19:34:49 +0900

    Update chap1-3_kelvinWeathier.js

--- chap1-3_kelvinWeathier.js
+++ chap1-3_kelvinWeathier.js
@@ -7,12 +7,12 @@
 
 // 만약에 kelvin온도가 0이라면?
 const kelvin = 0;
-let celsius = kelvin - 273 ; // 섭씨온도는 kelvin - 273
-let fahrenheit = Math.floor(celsius*(9/5) + 32); // 화씨온도는 섭씨*(9/5) + 32
+const celsius = kelvin - 273 ; // 섭씨온도는 kelvin - 273
+const fahrenheit = Math.floor(celsius*(9/5) + 32); // 화씨온도는 섭씨*(9/5) + 32
 console.log(`오늘 화씨 온도는 ${fahrenheit} 입니다.`);
 
 //Newton의 값을 구해보자
-let newton = Math.floor(celsius*(33/100));
+const newton = Math.floor(celsius*(33/100));
 console.log(`오늘 Newton의 값은 ${newton} 입니다.`)
 
 
