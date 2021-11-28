216aee9b9efab119d710876fc34d643eccc4aa26
JaewanK <37770843+JaewanK@users.noreply.github.com>
2021-11-28 21:07:00 +0900

    Create chap1-1_intro.js

---
+++ chap1/chap1-1_intro.js
@@ -0,0 +1,13 @@
+// introduction JavaScript
+
+// Properties - length
+console.log('Hello'.length);
+
+// Method - Object의 Method..?
+// Method는 함수같은거..라서 Properties 사용과는 구분이 필요해 보임
+
+console.log('hello'); //Method이기 때문에 parentheses가 필요함
+
+// Built-in Objects - Method를 가진 Object
+console.log(Math.random());
+
